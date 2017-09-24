f355cf872a5e18ce4753c04e70b11a1f2dfdd6f2
Juli <july1995bolt@gmail.com>
2017-09-24 12:33:05 -0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/') # define la ruta con la que se ingresa en el explorador
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run (host = '127.0.0.1', port = 5000)
