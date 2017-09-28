106d8a9eecff60f46abebc1e3db623a4c69f9b37
Juli <july1995bolt@gmail.com>
2017-09-28 19:44:06 -0300

    Create functions

--- __init__.py
+++ __init__.py
@@ -1,9 +1,50 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import time
 
 app = Flask(__name__)
-app.debug = True
+#app.debug = True
+
+def lector_datos():
+    ser = serial.Serial('COM4', 9600, timeout=0)
+    while 1:
+        try:
+            print ser.readline()
+        except ser.SerialTimeoutException:
+            print('No se pueden leer los datos')
+    time.sleep(1)
+
+
+@app.route('/datos')
+def cargarImagen():
+    filas = 3
+    columnas = 4
+
+    with open("datos.html", "w") as file:
+        file.write('<!DOCTYPE html><html><style>.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 120px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 1s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:hover .tooltiptext {visibility: visible;opacity: 1;}</style><head><link type="text/css" rel="stylesheet" href="custom.css" media="all"></head><body>')
+
+    temp = [2,4,8,3],[6,1,7,1],[12,1,2,6.3]
+    '''[1.5,1,1,1]
+       [6,1,2,1]
+       [12,1,2,1]'''
+
+    plt.imshow(temp, cmap='hot') #Create image with scale colors
+    plt.colorbar() #Draw color bar
+    plt.savefig('imagen.png', bbox_inches='tight')
+    #plt.show()  #Show image 
+
+    for i in xrange (0,filas): # i = columnas
+        for j in xrange (0,columnas): # j = filas
+            print temp[i][j]
+            with open("datos.html", "a") as file:
+                file.write('<div class="tooltip" style=" border: none; position: absolute; width: 100px; height: 100px; left: {1}px; top:{0}px;"><span class="tooltiptext">{2}</span></div>'.format(i*100+58,j*100+62,temp[i][j]))
+    with open("datos.html", "a") as file:
+        file.write('<img src="imagen.png" alt="" /></body></html>')
+    return 'Imagen Creada con Exito'
 
 @app.route('/') # define la ruta con la que se ingresa en el explorador
 def index():
