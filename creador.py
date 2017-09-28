6e3da273345653a8c7b291fb1d38d67536605acb
Juli <july1995bolt@gmail.com>
2017-09-28 18:28:55 -0300

    Add image pos

--- creador.py
+++ creador.py
@@ -9,20 +9,20 @@ columnas = 4
 with open("datos.html", "w") as file:
     file.write('<!DOCTYPE html><html><style>.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 120px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 1s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:hover .tooltiptext {visibility: visible;opacity: 1;}</style><head><link type="text/css" rel="stylesheet" href="custom.css" media="all"></head><body>')
 
-temp = [1.5,1,1,1],[6,1,2,1],[12,1,2,1]
+temp = [2,4,8,3],[6,1,7,1],[12,1,2,6.3]
 '''[1.5,1,1,1]
    [6,1,2,1]
    [12,1,2,1]'''
 
 plt.imshow(temp, cmap='hot') #Create image with scale colors
-#plt.colorbar() #Draw color bar
-#plt.savefig('imagen.png', bbox_inches='tight')
+plt.colorbar() #Draw color bar
+plt.savefig('imagen.png', bbox_inches='tight')
 #plt.show()  #Show image 
 
 for i in xrange (0,filas): # i = columnas
     for j in xrange (0,columnas): # j = filas
         print temp[i][j]
         with open("datos.html", "a") as file:
-            file.write('<div class="tooltip" style=" background-color: red; position: absolute; width: 100px; height: 100px; left: {1}px; top:{0}px;"><span class="tooltiptext">{2}</span></div>'.format(i*100,j*100,temp[i][j]))
+            file.write('<div class="tooltip" style=" border: none; position: absolute; width: 100px; height: 100px; left: {1}px; top:{0}px;"><span class="tooltiptext">{2}</span></div>'.format(i*100+58,j*100+62,temp[i][j]))
 with open("datos.html", "a") as file:
-    file.write('</body></html>') 
+    file.write('<img src="imagen.png" alt="" /></body></html>') 
