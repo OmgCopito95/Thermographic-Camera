11f49960c70ccd2f24d3b1e5ad991bf10013ee96
Juli <july1995bolt@gmail.com>
2017-09-29 00:46:18 -0300

    Add html_variables

--- creador.py
+++ creador.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+from html_variables import *
 
 
 filas = 3
 columnas = 4
 
 with open("datos.html", "w") as file:
-    file.write('<!DOCTYPE html><html><head><meta charset="utf-8"><link type="text/css" rel="stylesheet" href="static/bootstrap/css/custom.css" media="all"></head><body>')
+    file.write('<!DOCTYPE html>{0}<body style="background: none;">'.format(HTML_HEAD))
 
 temp = [2,4,8,3],[6,1,7,1],[12,1,2,6.3]
 '''[1.5,1,1,1]
@@ -24,6 +25,6 @@ for i in xrange (0,filas): # i = columnas
     for j in xrange (0,columnas): # j = filas
         print temp[i][j]
         with open("datos.html", "a") as file:
-            file.write('<div class="tooltip" style=" border: none; position: absolute; width: 100px; height: 100px; left: {1}px; top:{0}px;"><span class="tooltiptext">{2}°C</span></div>'.format(i*100+58,j*100+62,temp[i][j]))
+            file.write('<div class="tooltip" style=" border: none; position: absolute; width: 100px; height: 100px; left: {1}px; top:{0}px;"><span class="tooltiptext">{2}°C</span></div>\n'.format(i*100+58,j*100+62,temp[i][j]))
 with open("datos.html", "a") as file:
-    file.write('<img src="imagen.png" alt="" /></body></html>') 
+    file.write('{0}</body></html>'.format(HTML_IMAGEN)) 
