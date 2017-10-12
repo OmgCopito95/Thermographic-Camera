011188604b1a01511f039838839b2ebffcd80be4
Juli <july1995bolt@gmail.com>
2017-10-12 13:57:06 -0300

    Create functions pages html

--- __init__.py
+++ __init__.py
@@ -6,12 +6,24 @@ import creador
 app = Flask(__name__)
 
 temp = [2,4,8,3],[6,1,7,1],[12,1,2,6.3]
-    '''[1.5,1,1,1]
-       [6,1,2,1]
-       [12,1,2,1]'''
+#	[1.5,1,1,1]
+#    [6,1,2,1]
+#    [12,1,2,1]
 
 def cargarImagen():
     creador.crear_html_imagen(temp)
+
+@app.route('/camara')
+def camara():
+	return render_template('camara.html')
+
+@app.route('/como-se-hace')
+def como-se-hace():
+	return render_template('como-se-hace.html')
+
+@app.route('/quien-soy')
+def quien-soy():
+	return render_template('quien-soy.html')
 
 @app.route('/') # define la ruta con la que se ingresa en el explorador
 def index():
