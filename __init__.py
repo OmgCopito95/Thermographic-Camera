75876433eff4550d5065d9988752d4d7f6bfb38d
Juli <july1995bolt@gmail.com>
2017-12-03 00:43:41 -0300

    Add lee_datos / camara var

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import creador
+import lector_datos as lector 
 
 app = Flask(__name__)
 
@@ -10,17 +11,40 @@ def cargarImagen():
 
 @app.route('/camara', methods = ['POST', 'GET'])
 def camara():
-    tempmin,tempmax,porcentaje,tiempo = leer_datos()
-    return render_template('camara.html',tempmin = temperaturamin,tempmax=temperaturamax,tiempo=tiempo,porcentaje=porcentaje)
-    
+    resultado=""
+    with open("temperaturas.txt", "r") as file:
+        lineas = file.readlines()
+    if lineas:
+        temperaturamin = lineas[0]
+        temperaturamax = lineas[1]
+        resultado = lineas[2]
+    else:
+        temperaturamin = 0
+        temperaturamax = 0
+    porcentaje = 100
+    tiempo = 5
+
+    if resultado == "fin":
+        ver_resultado = ''' href=/resultado '''
+    else:
+        ver_resultado = '''disabled style="cursor: not-allowed";'''
+    return render_template('camara.html',resultado=ver_resultado,tempmin = temperaturamin,tempmax=temperaturamax,tiempo=tiempo,porcentaje=porcentaje)
+    #return render_template('camara.html')
 
 @app.route('/como-se-hace')
 def comoSeHace():
-    #cargarImagen()
     return render_template('como-se-hace.html')
 
 def cargarImagen():
     creador.crear_html_imagen()
+
+@app.route('/lee_datos')
+def lee_datos(): # Se utiliza para que ejecute la funcion leer datos
+    lector.iniciar()
+    lector.leer()
+    lector.verificar()
+    cargarImagen()
+    return render_template('lee_datos.html')
 
 @app.route('/quien-soy')
 def quienSoy():
@@ -36,4 +60,3 @@ def index():
 
 if __name__ == "__main__":
     app.run (host = '127.0.0.1', port = 5000)
-    
